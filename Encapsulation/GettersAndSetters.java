17823db4df7e44be444391b8cf749703213e6987
Shardul Gusain <shardulsgusain1@gmail.com>
2024-03-28 08:11:38 +0530

    getters and setters

---
+++ Encapsulation/GettersAndSetters.java
@@ -0,0 +1,25 @@
+package Encapsulation;
+
+class Human
+{
+    int age;
+    String name;
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+public class GettersAndSetters {
+    public static void main(String[] args) {
+        
+    }
+    //Standard that developers follow to get and set values of instance values.
+}
