c06f2b639a45b25be04b0067ef903cccfc8d5cd3
Shardul Gusain <shardulsgusain1@gmail.com>
2024-03-28 20:11:56 +0530

    final keyword use in java

---
+++ FinalKeyword/FinalKeyword.java
@@ -0,0 +1,11 @@
+package FinalKeyword;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        //Final variables cant be re assigned
+        //Final classes cant be inherited/extended
+        //Final methods cant be overriden
+        final int a = 10;
+
+    }
+}
---
+++ Polymorphism/Polymorphism.java
@@ -0,0 +1,38 @@
+package Polymorphism;
+
+class A 
+{
+    public void show()
+    {
+        System.out.println("In class A");
+    }
+}
+
+class B extends A 
+{
+    public void show()
+    {
+        System.out.println("In class B");
+    }
+}
+
+class C extends A 
+{
+    public void show()
+    {
+        System.out.println("In class C");
+    }
+}
+
+
+
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.show();
+
+        obj = new C();
+        obj.show();
+    }
+}
