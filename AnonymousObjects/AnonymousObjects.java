dcc40e8fb6bf9c276f3713dcdb89545e5e611eef
Shardul Gusain <shardulsgusain1@gmail.com>
2024-03-28 11:01:16 +0530

    anonymous objects in java

---
+++ AnonymousObjects/AnonymousObjects.java
@@ -0,0 +1,13 @@
+package AnonymousObjects;
+
+class Human{
+     public Human()
+     {
+        System.out.println("Human object is created in heap memory of JVM");
+     }
+}
+public class AnonymousObjects {
+    public static void main(String[] args) {
+        new Human();
+    }
+}
