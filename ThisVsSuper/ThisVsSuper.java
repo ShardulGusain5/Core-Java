c9e7b77b1ec22c44192e2bb91b7cba44a483a438
Shardul Gusain <shardulsgusain1@gmail.com>
2024-03-28 18:09:10 +0530

    this vs super

--- ThisVsSuper/ThisVsSuper.java
+++ ThisVsSuper/ThisVsSuper.java
@@ -1,27 +1,38 @@
 package ThisVsSuper;
 
-class A
-{
-    A()
-    {
+class A {
+    A() {
         System.out.println("this is A class");
     }
-    A(int n)
-    {
+
+    A(int n) {
         System.out.println("This is A int");
     }
 }
-class B extends A
-{
-    B()
-    {
+
+class B extends A {
+    B() {
         super(10);
         System.out.println("this is B constructor");
     }
 }
+
+class C extends A 
+{
+    C()
+    {
+        System.out.println("i am in C");
+    }
+    C(int x)
+    {
+        System.out.println("I ma C 2");
+    }
+}
+
 public class ThisVsSuper {
     public static void main(String[] args) {
-        B  obj = new B();
-        
+        B obj = new B();
+        C obj1 = new C(10);
+
     }
 }
