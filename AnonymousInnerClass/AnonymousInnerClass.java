62bde7f1685778f969e2f1216be09ba7dc743b52
Shardul Gusain <shardulsgusain1@gmail.com>
2024-03-30 17:07:47 +0530

    Anonymous Inner Class

---
+++ AnonymousInnerClass/AnonymousInnerClass.java
@@ -0,0 +1,18 @@
+package AnonymousInnerClass;
+
+class A {
+    public void show() {
+        System.out.println("In A show");
+    }
+}
+
+public class AnonymousInnerClass {
+    public static void main(String[] args) {
+        A obj = new A() {
+            public void show() {
+                System.out.println("In new implementation of show");
+            }
+        };
+        obj.show();
+    }
+}
