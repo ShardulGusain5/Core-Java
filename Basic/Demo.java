293d310e20dab8e431966e86ba0c3e530e06ef13
Shardul Gusain <shardulsgusain1@gmail.com>
2024-04-15 07:34:02 +0530

    class and objects

---
+++ Basic/Demo.java
@@ -0,0 +1,15 @@
+class Box
+{
+    int side;
+    Box()
+    {
+        System.out.println("Box is cube");
+    }
+}
+
+
+public class Demo {
+    public static void main(String[] args) {
+        Box B = new Box();
+    }
+}
