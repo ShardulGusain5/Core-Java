457fc95f8cdf0000c5feb3ad94c3c7a9e6e253d8
Shardul Gusain <shardulsgusain1@gmail.com>
2024-04-20 11:35:53 +0530

    fix

--- Basic/Demo.java
+++ Basic/Demo.java
@@ -5,11 +5,16 @@ class Box
     {
         System.out.println("Box is cube");
     }
+    Box(String name )
+    {
+        System.out.println("Box is "+ name);
+    }
 }
 
 
 public class Demo {
     public static void main(String[] args) {
         Box B = new Box();
+        Box B1= new Box("Cuboid");
     }
 }
