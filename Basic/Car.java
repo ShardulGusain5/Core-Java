9886b42d5ece4c9faa1346517603e04ade5f3fb3
Shardul Gusain <shardulsgusain1@gmail.com>
2024-04-24 19:49:36 +0530

    core java

--- Basic/Car.java
+++ Basic/Car.java
@@ -1,5 +1,12 @@
 class SuperCar
 {
+    public SuperCar(){
+
+    }
+    public SuperCar(String car)
+    {
+        System.out.println(car+" is running");
+    }
     public void run()
     {
         System.out.println("super car is running");
@@ -9,6 +16,7 @@ class SuperCar
 public class Car {
     public static void main(String[] args) {
         SuperCar S = new SuperCar();
+        SuperCar S2 = new SuperCar("Maruti");
         S.run();
     }
 }
