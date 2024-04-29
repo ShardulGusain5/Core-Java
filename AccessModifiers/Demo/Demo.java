f38839a79ff3da642c90e975bbc990db5c510d82
Shardul Gusain <shardulsgusain1@gmail.com>
2024-04-29 18:46:20 +0530

    access modifiers

--- AccessModifiers/Demo/Demo.java
+++ AccessModifiers/Demo/Demo.java
@@ -5,6 +5,7 @@ import AccessModifiers.Sample;
 public class Demo {
     public static void main(String[] args) {
         Sample S = new Sample();
+        //S.name//This is not accessible
         S.marks = 10;//As the variable is outside of this package hence need to be declared public variable to be accessible.
     }
 }
--- AccessModifiers/Sample.java
+++ AccessModifiers/Sample.java
@@ -2,4 +2,5 @@ package AccessModifiers;
 
 public class Sample {
     public int marks;
+    private String name = "Shardul";
 }
