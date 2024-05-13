6f8645d9d7a2d83db18989791ee5cb2405c4db18
Shardul Gusain <shardulsgusain1@gmail.com>
2024-05-13 20:09:42 +0530

    final keyword in java

--- FinalKeyword/FinalKeyword.java
+++ FinalKeyword/FinalKeyword.java
@@ -1,11 +1,40 @@
 package FinalKeyword;
 
-public class FinalKeyword {
-    public static void main(String[] args) {
-        //Final variables cant be re assigned
-        //Final classes cant be inherited/extended
-        //Final methods cant be overriden
-        final int a = 10;
 
-    }
-}
+// /**
+//  * InnerFinalKeyword
+//  */
+// class InnerFinalKeyword {
+
+//     public final void greet()
+//     {
+//         System.out.println("hello shardul");
+//     }
+// }
+
+// /**
+//  *  Demo */
+// class Demo1 extends InnerFinalKeyword {   
+//     public  void greet()//Cannot be overriden and hence this will throw error
+//     {
+//         System.out.println("hello shardul");
+//     }
+
+    
+// }
+
+
+// public class FinalKeyword {
+//     public static void main(String[] args) {
+//         //Final variables cant be re assigned
+//         //Final classes cant be inherited/extended
+//         //Final methods cant be overriden
+//         final int a = 10;
+
+//         Demo1 D = new Demo1();
+//         D.greet();
+
+
+
+//     }
+// }
