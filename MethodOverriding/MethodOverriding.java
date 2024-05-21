6eef8d81cf0590a1fc47ee193a5bd819ea4abae1
Shardul Gusain <shardulsgusain1@gmail.com>
2024-05-21 17:13:49 +0530

    method overriding

--- MethodOverriding/MethodOverriding.java
+++ MethodOverriding/MethodOverriding.java
@@ -16,5 +16,10 @@ public class MethodOverriding {
     public static void main(String[] args) {
         B obj = new B();
         obj.show();
+        A obj1 = new A();
+        obj1.show();
+
+        A obj2 = new B();
+        obj2.show();
     }
 }
