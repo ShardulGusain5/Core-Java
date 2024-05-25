9e3c04acd4b9637cc0376bf9359d8473fb9ec5cd
Shardul Gusain <shardulsgusain1@gmail.com>
2024-05-25 12:26:37 +0530

    problem solving prime till n

---
+++ ProblemSolving/PrimeTillN.java
@@ -0,0 +1,30 @@
+package ProblemSolving;
+
+import java.util.Scanner;
+
+public class PrimeTillN {
+    public static void main(String[] args) {
+        int n = 36;
+
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int primeCount = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                primeCount++;
+            }
+        }
+        System.out.println(primeCount);
+    }
+}
