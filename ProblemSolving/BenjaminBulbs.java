0922d9447f3fd42a7eb8dfb67aa41f40762ac8ca
Shardul Gusain <shardulsgusain1@gmail.com>
2024-05-26 15:19:04 +0530

    benjamin bulbs

---
+++ ProblemSolving/BenjaminBulbs.java
@@ -0,0 +1,11 @@
+package ProblemSolving;
+
+public class BenjaminBulbs {
+    public static void main(String[] args) {
+        int n =20;
+        for(int i =1;i*i<=n;i++)
+        {
+            System.out.println(i*i);
+        }
+    }
+}
