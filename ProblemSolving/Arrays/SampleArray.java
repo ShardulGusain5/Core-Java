f0459e3203cd71edff882ef81de6e37511595be5
Shardul Gusain <shardulsgusain1@gmail.com>
2024-05-27 19:22:00 +0530

    span of array

---
+++ ProblemSolving/Arrays/SampleArray.java
@@ -0,0 +1,11 @@
+package ProblemSolving.Arrays;
+
+public class SampleArray {
+    public static void main(String[] args) {
+        int [] arr = {1,5,6};
+        for(int i =0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ ProblemSolving/Arrays/SpanOfArray.java
@@ -0,0 +1,22 @@
+package ProblemSolving.Arrays;
+//Span of array = max-min
+public class SpanOfArray {
+    public static void main(String[] args) {
+        int arr[] = {1,8,92,7,3,5};
+        int max=arr[0];
+        int min=arr[0];
+
+        for(int i =1;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min = arr[i];
+            }
+            else if(arr[i]>max)
+            {
+                max = arr[i];
+            }
+        }
+        System.out.println(max-min);
+    }
+}
