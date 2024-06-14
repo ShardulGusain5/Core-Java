aaece8961fe5c38fc1a1673d011b96da2d0ec263
Shardul Gusain <shardulsgusain1@gmail.com>
2024-06-14 06:38:53 +0530

    linear search

---
+++ DivideAndConquer/LinearSearch.java
@@ -0,0 +1,25 @@
+package DivideAndConquer;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 0, 1, 9};
+        int target = 1;
+        int result = linearSearch(arr, target);
+        
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Return the index of the found element
+            }
+        }
+        return -1; // Return -1 if the element is not found
+    }
+}
+
